8a1900a87b6f0fdb5ccc6c5fb8611c89c1b82af2
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-23 16:18:05 +0200

    useState használat

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  //let title = props.title;
+
   const clickHandler = () => {
-    console.log("Clicked");
+    setTitle("Updated");
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}Ft</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
