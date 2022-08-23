614674b9e6d3eab4d8b7c03bb1aae7006ee90248
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-23 12:38:26 +0200

    css hozzáadása a componentekhez

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2> Title</h2>
-        <div>Amount</div>
+    <div className="expense-item">
+      <div>2022 March 17.</div>
+      <div className="expense-item__description">
+        <h2> Biztosítás</h2>
+        <div className="expense-item__price">80000 Ft</div>
       </div>
     </div>
   );
