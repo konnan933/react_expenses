465879b60feaea606b9b407da7a0e1a503e3e893
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-23 12:53:05 +0200

    palceholders létrehozva

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 3, 17);
+  const expenseItem = "Biztosítás";
+  const expenseAmount = 80000;
   return (
     <div className="expense-item">
-      <div>2022 March 17.</div>
+      <div>{expenseDatetoISOString()}</div>
       <div className="expense-item__description">
-        <h2> Biztosítás</h2>
-        <div className="expense-item__price">80000 Ft</div>
+        <h2>{expenseItem}</h2>
+        <div className="expense-item__price">{expenseAmount}Ft</div>
       </div>
     </div>
   );
