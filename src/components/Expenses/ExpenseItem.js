5aaa47ae00c249cf36cee3b602165b39822d1433
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-23 18:38:34 +0200

    Formatting ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,15 @@
-import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  //let title = props.title;
-
-  const clickHandler = () => {
-    setTitle("Updated");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}Ft</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
