07cf272711f1d28acced0c962d453276f692b275
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-23 12:29:21 +0200

    complex component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expenses!</h2>;
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2> Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
