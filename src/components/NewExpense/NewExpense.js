c30981e74f71388b3ed01fb9f6162300ffd9a7b9
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-24 12:25:50 +0200

    Chart fáljok hozzáadva és form fejlesztve

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,37 @@
+import react, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   function SaveExpensData(enteredExpensData) {
     const expenseData = {
       ...enteredExpensData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  }
+
+  function startEditingHnadler() {
+    setIsEditing(true);
+  }
+  function stopEditingHandler() {
+    setIsEditing(false);
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpensData={SaveExpensData} />
+      {!isEditing && (
+        <button onClick={startEditingHnadler}>Add new Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpensData={SaveExpensData}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
